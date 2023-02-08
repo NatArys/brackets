6a62c08686f607127c49782e2fa9fe7f67b92d31
NatArys <91931609+NatArys@users.noreply.github.com>
2023-02-08 12:03:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsObj = Object.fromEntries(bracketsConfig);
+  let stack = []
+
+  for (i = 0; i < str.length; i++) {
+
+    if (str[i] in bracketsObj) {
+      stack.push(str[i]);
+    } else {
+      if (bracketsObj[stack[stack.length - 1]] == str[i]) {
+        stack.pop();
+      } else return false;
+    }
+  }
+ return stack.length ==0;
 }
